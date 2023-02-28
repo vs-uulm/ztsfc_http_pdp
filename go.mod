70b82d8828af0c41b02bbd1620dc9aa2ec43a81b
leobrada <leo.bradatsch@gmail.com>
2023-02-28 21:33:00 +0100

    allowed services for users added

--- go.mod
+++ go.mod
@@ -3,15 +3,15 @@ module github.com/vs-uulm/ztsfc_http_pdp
 go 1.17
 
 require (
-	github.com/leobrada/golang_convenience_tools v0.0.0-20220404052613-f43df838ca46
+	github.com/leobrada/golang_convenience_tools v0.0.0-20230226145447-cbd5896c5ecf
 	github.com/leobrada/yaml_tools v0.0.0-20220115205103-7f6e1de7ab2e
-	github.com/vs-uulm/ztsfc_http_attributes v0.0.0-20220605171037-b8029bf0147e
+	github.com/vs-uulm/ztsfc_http_attributes v0.0.0-20230228194947-f173ec42fec4
 	github.com/vs-uulm/ztsfc_http_logger v0.0.0-20220504121928-852f30c337e5
-	golang.org/x/time v0.0.0-20220411224347-583f2d630306
+	golang.org/x/time v0.3.0
 )
 
 require (
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/policy_engine/policy_engine.go
+++ internal/app/policy_engine/policy_engine.go
@@ -1,28 +1,46 @@
 package policy_engine
 
 import (
+	md "github.com/vs-uulm/ztsfc_http_pdp/internal/app/metadata"
     logger "github.com/vs-uulm/ztsfc_http_logger"
     rattr "github.com/vs-uulm/ztsfc_http_attributes"
 )
 
 // TODO: Policy Missing. Harcoded ATM
-func EvaluateCriteriaBasedPolicyRules(sysLogger *logger.Logger, user *rattr.User, device *rattr.Device, system *rattr.System) (authDecision bool, feedback string) {
+func EvaluateCriteriaBasedPolicyRules(sysLogger *logger.Logger, cpm *md.Cp_metadata, user *rattr.User, device *rattr.Device, system *rattr.System) (authDecision bool, feedback string) {
 	if user != nil && len(user.UserID) == 0 {
 		sysLogger.Infof("authorization: PerformAuthorization(): Requested was rejected since the involved user is not present in the user DB")
 		authDecision = false
 		feedback = "Your request was rejected since your user is not managed by the user DB"
 		return
 	}
+
 	if device != nil && len(device.DeviceID) == 0 {
 		sysLogger.Infof("authorization: PerformAuthorization(): Requested was rejected since the involved device is not present in the device DB")
 		authDecision = false
 		feedback = "Your request was rejected since your device is not managed by the device DB"
 		return
 	}
+
 	if device != nil && device.Revoked {
 		sysLogger.Infof("authorization: PerformAuthorization(): Requested was rejected since the involved device '%s' is revoked", device.DeviceID)
 		authDecision = false
 		feedback = "Your request was rejected since your device is revoked"
+		return
+	}
+
+	allowedToAccessService := false
+	for _, service := range user.AllowedServices {
+		if cpm.Resource == service {
+			allowedToAccessService = true
+			continue
+		}
+	}
+	if !allowedToAccessService {
+		sysLogger.Infof("authorization: PerformAuthorization(): Requested was rejected since the involved user '%s' is not allowed to access the requested service",
+			user.UserID)
+		authDecision = false
+		feedback = "Your request was rejected since you are not allowed to access the requested service"
 		return
 	}
 
