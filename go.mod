3dd2354956f424e60a7946913caff1f8ec8748fd
leobrada <leo.bradatsch@gmail.com>
2023-05-14 21:33:43 +0200

    trust algorithm added to policies

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.17
 require (
 	github.com/leobrada/golang_convenience_tools v0.0.0-20230226145447-cbd5896c5ecf
 	github.com/leobrada/yaml_tools v0.0.0-20220115205103-7f6e1de7ab2e
-	github.com/vs-uulm/ztsfc_http_attributes v0.0.0-20230228194947-f173ec42fec4
+	github.com/vs-uulm/ztsfc_http_attributes v0.0.0-20230514081946-84db18db718d
 	github.com/vs-uulm/ztsfc_http_logger v0.0.0-20220504121928-852f30c337e5
 	golang.org/x/time v0.3.0
 )
 
 require (
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/policies/policies.go
+++ internal/app/policies/policies.go
@@ -1,78 +1,83 @@
 package policies
 
 import (
-    "fmt"
-    "net"
-    "time"
+	"fmt"
+	"net"
+	"time"
 
-    "golang.org/x/time/rate"
+	"golang.org/x/time/rate"
 )
 
 var (
-    Policies PoliciesT
+	Policies PoliciesT
 )
 
+type TrustAlgorithmT struct {
+	Variant string `yaml:"variant"`
+}
+
 type AttributesT struct {
-    User UserT  `yaml:"user"`
-    Device DeviceT  `yaml:"device"`
-    Sf map[string]*SfT `yaml:"sf"`
+	User   UserT           `yaml:"user"`
+	Device DeviceT         `yaml:"device"`
+	Sf     map[string]*SfT `yaml:"sf"`
 }
 
 type UserT struct {
-    PwAuthenticated    int `yaml:"pw_authenticated"`
-    UsualTime   int `yaml:"usual_time"`
-    UsualService    int `yaml:"usual_service"`
+	PwAuthenticated int `yaml:"pw_authenticated"`
+	UsualTime       int `yaml:"usual_time"`
+	UsualService    int `yaml:"usual_service"`
 }
 
 type DeviceT struct {
-    CertAuthenticated   int `yaml:"cert_authenticated"`
-    FromTrustedLocation int `yaml:"from_trusted_location"`
-    WithinAllowedRequestRate int `yaml:"within_allowed_request_rate"`
+	CertAuthenticated        int `yaml:"cert_authenticated"`
+	FromTrustedLocation      int `yaml:"from_trusted_location"`
+	WithinAllowedRequestRate int `yaml:"within_allowed_request_rate"`
 }
 
 type SfT struct {
-    Basic int `yaml:"basic"`
-    Advanced int `yaml:"advanced"`
-    Full int `yaml:"full"`
+	Basic    int `yaml:"basic"`
+	Advanced int `yaml:"advanced"`
+	Full     int `yaml:"full"`
 }
 
 type ActionT struct {
-    TrustThreshold int `yaml:"trust_threshold"`
+	TrustThreshold int `yaml:"trust_threshold"`
 }
 
 type ResourceT struct {
-    TargetSensitivity int `yaml:"target_sensitivity"`
-    ProtocolSecurity int `yaml:"protocol_security"`
-    TargetState int `yaml:"target_state"`
-    TargetHealth int `yaml:"target_health"`
-    TargetVuln int `yaml:"target_vuln"`
-    Actions map[string]*ActionT `yaml:"actions"`
-    TrustedLocations   []string `yaml:"trusted_locations"`
-    AllowedRequestsPerSecond rate.Limit `yaml:"allowed_requests_per_second"`
-    AllowedDevicesPerUser int `yaml:"allowed_devices_per_user"`
+	TargetSensitivity        int                 `yaml:"target_sensitivity"`
+	ProtocolSecurity         int                 `yaml:"protocol_security"`
+	TargetState              int                 `yaml:"target_state"`
+	TargetHealth             int                 `yaml:"target_health"`
+	TargetVuln               int                 `yaml:"target_vuln"`
+	Actions                  map[string]*ActionT `yaml:"actions"`
+	TrustedLocations         []string            `yaml:"trusted_locations"`
+	AllowedRequestsPerSecond rate.Limit          `yaml:"allowed_requests_per_second"`
+	AllowedDevicesPerUser    int                 `yaml:"allowed_devices_per_user"`
 
-    TrustedIPNetworks []*net.IPNet
-    ResourceAccessLimits map[string]map[string]*AccessLimiter
+	TrustedIPNetworks    []*net.IPNet
+	ResourceAccessLimits map[string]map[string]*AccessLimiter
 }
 
 type AccessLimiter struct {
-    AccessLimit *rate.Limiter
-    PenaltyTimestamp time.Time
+	AccessLimit      *rate.Limiter
+	PenaltyTimestamp time.Time
 }
 
 type PoliciesT struct {
-    Attributes AttributesT  `yaml:"attributes"`
-    Resources map[string]*ResourceT  `yaml:"resources"`
+	TrustAlgorithm TrustAlgorithmT       `yaml:"trust_algorithm"`
+	Attributes     AttributesT           `yaml:"attributes"`
+	Resources      map[string]*ResourceT `yaml:"resources"`
 }
 
 func PrintAllowedRequestsPerSecond(p *PoliciesT) {
-    for key, val := range p.Resources {
-        fmt.Printf("Allowed requests per second for %s: %f\n", key, val.AllowedRequestsPerSecond)
-    }
+	for key, val := range p.Resources {
+		fmt.Printf("Allowed requests per second for %s: %f\n", key, val.AllowedRequestsPerSecond)
+	}
 }
 
 func PrintTrustedLocations(p *PoliciesT) {
-    for key, val := range p.Resources {
-        fmt.Printf("Trusted locations for %s: %v", key, val.TrustedLocations)
-    }
+	for key, val := range p.Resources {
+		fmt.Printf("Trusted locations for %s: %v", key, val.TrustedLocations)
+	}
 }
